d24001ad0860c09d0433705d08bfa4e4ac589113
Rahul Zore <zore.r@husky.neu.edu>
2020-02-08 15:27:36 -0800

    Completed Shop page, colletions preview and colletions item component

--- src/App.js
+++ src/App.js
@@ -3,19 +3,14 @@ import './App.css';
 import  { Route, Switch } from 'react-router-dom';
 
 import Homepage from './pages/homepage/homepage.component';
-
-const Hatspage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+import ShopPage from './pages/shop/shop.componenet';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Homepage}/>
-        <Route path='/shop/hats' component={Hatspage}/>
+        <Route path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
