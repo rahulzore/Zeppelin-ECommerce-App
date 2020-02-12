0d3a2d077aa48ef836887e8e259b94b339c54598
Rahul Zore <zore.r@husky.neu.edu>
2020-02-12 01:53:37 -0800

    added AddItem to cart reducer and grouping similar cart items

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -12,8 +12,8 @@ const CollectionPreview = ({title, items}) => {
                 {
                     items
                     .filter((item, idx) => idx <4)
-                    .map(({id, ...otherItemProps}) => {
-                        return <CollectionItem key={id} {...otherItemProps}/>
+                    .map(item => {
+                        return <CollectionItem key={item.id} item={item}/>
                     })
                 }
             </div>
