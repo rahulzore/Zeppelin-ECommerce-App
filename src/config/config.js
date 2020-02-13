6ecac90101b985097a3cd6af66be854c4a92b63a
Rahul Zore <zore.r@husky.neu.edu>
2020-02-12 19:10:26 -0800

    Minor tweaks

---
+++ src/config/config.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAcorL5Eyr1wTxmpxmiWr0SYuorvoq8Lpo",
+    authDomain: "zeppelin-ecommerce-app.firebaseapp.com",
+    databaseURL: "https://zeppelin-ecommerce-app.firebaseio.com",
+    projectId: "zeppelin-ecommerce-app",
+    storageBucket: "zeppelin-ecommerce-app.appspot.com",
+    messagingSenderId: "240575178485",
+    appId: "1:240575178485:web:5169558a4bc23bf21887bc",
+    measurementId: "G-RZPL87B2NJ"
+  };
+
+export const StripeKey = 'pk_test_QG8mVCDLOSHaADPD6vHMHRe400wqNG4CAm'
+
+export default firebaseConfig;
