3541e85c0ed673cc310ed401d813d311e0b87f5b
Rahul Zore <zore.r@husky.neu.edu>
2020-02-12 17:06:09 -0800

    Collection page changes

---
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './collections-overview.styles.scss';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect'
+import CollectionPreview from '../collection-preview/collection-preview.component';
+import { selectCollectionForPreview } from '../../redux/shop/shop.selectors';
+
+const CollectionsOverview = ({collections}) => (
+    <div className='collections-overview'>
+    {
+        collections.map(({id, ...otherCollectionProps}) => (
+            <CollectionPreview key={id} {...otherCollectionProps}/>
+        ))
+    }
+    </div>
+)
+
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollectionForPreview
+})
+
+export default connect(mapStateToProps)(CollectionsOverview);
